c4450bf391bc448afa2a698cc4acbd664e3f96be
Maxim Doronin <doro156@me.com>
2015-11-24 23:50:21 +0300

    Class of lists has been implemented

--- include/list.h
+++ include/list.h
@@ -1,4 +1,7 @@
 #pragma once
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
 
 struct NODE
 {
@@ -6,6 +9,30 @@ struct NODE
 	NODE *pNext;
 };
 
-void negativeInPlace(NODE *&pFirst);
-NODE* negativeCopy(NODE *&pFirstSourse);
-void EvenOdd(NODE *&pFirst, NODE *&pFirstOdd, NODE *&pFirstEven);
+
+class List {
+private: 
+	NODE* pFirst;
+public:
+	List(int count);
+	~List();
+	
+	void print();
+	NODE* find(int key);
+	int findPrev(int key, NODE *&point, NODE *&pPrev);
+	
+	void insertionFirst(int key);
+	void insertionEnd(int key);
+	void insertionPrev(int key, NODE *&elem);
+	void insertionNext(int key, NODE *&elem);
+
+	int del(int key);
+private:
+	int del(NODE *elem);
+public:
+	void negativeInPlace();
+	List* negativeCopy();
+	void EvenOdd(List *&pFirstOdd, List *&pFirstEven);
+
+	void sort();
+};
--- sample/sample.cpp
+++ sample/sample.cpp
@@ -1,45 +1,9 @@
 #include "list.h"
-#include <cstdlib>
-#include <iostream>
-#include <ctime>
 
 int main()
 {
-	NODE *list = new NODE;
-	NODE *tmp = list;
-	srand(time(0));
-	for(int i=0; i<5; i++)
-	{
-		tmp->key = 50 - rand()%100;
-		std::cout << tmp->key << " ";
-		tmp->pNext = new NODE;
-		tmp = tmp->pNext;
-	}
-	tmp->pNext = 0;
-	
-	
-	NODE *even = 0;
-	NODE *odd = 0;
-
-	EvenOdd(list, odd, even);
-
-	std::cout << "\n";
-
-	while (even != 0)
-	{
-		std::cout << even->key<< " ";
-		even = even->pNext;
-	}
-
-	std::cout << "\n";
-
-	while (odd != 0)
-	{
-		std::cout << odd->key<< " ";
-		odd = odd->pNext;
-	}
-
-	std::cout << "\n";
-
-	return 0;
+	List list(10);
+	list.print();
+	list.negativeInPlace();
+	list.print();
 }
